a375f9a42b7485000b5dc0122b9e285b7cb05cfc
Kevin Harrington <mad.hephaestus@gmail.com>
2019-07-28 21:39:25 -0400

    adding name

--- launch.groovy
+++ launch.groovy
@@ -1,7 +1,7 @@
 def cat =ScriptingEngine.gitScriptRun(	"https://github.com/OperationSmallKat/SmallKat_V2.git", 
 								"loadRobot.groovy", 
 ["https://github.com/OperationSmallKat/greycat.git",
-		"MediumKat.xml","GameController_22"]);
+		"MediumKat.xml","GameController_22","hidDevice"]);
 println "Cat loaded, searching for game controller"
 def gameController =null
 try{
